d8f775cddfe5a2ea666976f1e3ae6114e31e60de
huawei.william <huawei.william@bytedance.com>
2021-07-31 17:05:58 +0800

    add googletest

--- tests/test_hellocov.cpp
+++ tests/test_hellocov.cpp
@@ -2,15 +2,13 @@
 //
 // Created by William.Hua on 2021/7/31.
 //
-
+#include <gmock/gmock.h>
 #include "test_lib.h"
-#include <cassert>
-int main()
-{
-       is_power_of_two(8);
-       multiply_by_two(7);
-       divide_by_two(-7);
-       is_power_of_two(-8);
 
-       return 0;
+TEST(ATest, Test)
+{
+    is_power_of_two(8);
+    multiply_by_two(7);
+    divide_by_two(-7);
+    is_power_of_two(-8);
 }
